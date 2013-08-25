a82c6ac22130cb9e344be8370d3c326a620e1d41
josiah samuel <josiahsams@gmail.com>
2013-08-25 15:31:53 +0530

    initial commit

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,5 @@
+class ListController < ApplicationController
+  def index
+    @time = Time.now     
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Test1::Application.config.secret_token = 'e3197885ea304a53ce65e95f47bd0f3036b12c1ca38af79008e6da188c09892e6cd75ac4ce213978d35cc1879bf3b5d0717f3a88501c0b05511c6bb0ef4fb3df'
